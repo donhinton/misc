11df9b131d2008b89ceb6a7ead27b3331ccd0c53
Don Hinton <hintonda@gmail.com>
2017-12-31 08:07:03 -0800

    Add stages.

--- cmake/linux-toolchain.cmake
+++ cmake/linux-toolchain.cmake
@@ -1,11 +1,7 @@
 # This toolchain file is intended to support cross compiling
 # clang+llvm for linux on Darwin.
-
-# This file is a WIP, and is a preliminary step in creating a 2-stage
-# workflow.  Assuming clang+llvm+lld have already been built on
-# Darwin, it can be invoked like this:
 #
-# PATH=<prefix to prebuilt clang+llvm+lld>/bin:$PATH cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=<path to this file>/linux-toolchain.cmake  ../../llvm
+# Usage: cmake -GNinja -C <path to this file>/linux-toolchain.cmake  ../llvm
 #
 # Currently requires the following patches still under review:
 #   https://reviews.llvm.org/D41621
@@ -13,74 +9,65 @@
 #   https://reviews.llvm.org/D41623
 #   https://gitlab.kitware.com/cmake/cmake/merge_requests/1620
 
-if(NOT DEFINED TOOLCHAIN_FILE_LOADED)
-  message(STATUS "Loading toolchain file: ${CMAKE_CURRENT_LIST_FILE}.")
-  set(TOOLCHAIN_FILE_LOADED TRUE CACHE BOOL "")
+# The sysroot tree was created via https://github.com/donhinton/misc/blob/master/scripts/export_docker_filesystem.sh
 
-  set(CMAKE_SYSTEM_NAME "Linux" CACHE STRING "")
+if(NOT DEFINED SYSROOT AND NOT DEFINED CMAKE_SYSROOT)
+	message(FATAL_ERROR "Missing required option -DSYSROOT=<sysroot path>.")
+endif()
 
-  set(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
-  set(LLVM_ENABLE_PROJECTS "clang;libcxx;libcxxabi;libunwind;lld" CACHE STRING "" FORCE)
+if(DEFINED SYSROOT)
+  set(LLVM_TARGETS_TO_BUILD X86 CACHE STRING "")
 
-  # All of these can be set in stage1 and passed via BOOTSTRAP_ args.
-  set(CMAKE_C_COMPILER_FORCED TRUE CACHE BOOL "" FORCE)
-  set(CMAKE_C_COMPILER "clang")
+  set(CMAKE_BUILD_TYPE RELEASE CACHE STRING "")
 
-  set(CMAKE_CXX_COMPILER_FORCED TRUE CACHE BOOL "" FORCE)
-  set(CMAKE_CXX_COMPILER "clang++")
+  set(LLVM_ENABLE_PROJECTS "clang;lld" CACHE STRING "")
+  set(CLANG_DEFAULT_LINKER "lld" CACHE STRING "")
 
-  find_program(CMAKE_RANLIB llvm-ranlib)
-  find_program(CMAKE_AR llvm-ar)
-  # When we start using this for a multi-stage build, these can be set
-  # to point to the first stage.
-  # find_program(CLANG_TABLEGEN clang-tblgen)
-  # find_program(LLVM_TABLEGEN llvm-tblgen)
-  # find_program(_LLVM_CONFIG_EXE llvm-config)
+  set(BOOTSTRAP_LLVM_ENABLE_LLD ON CACHE BOOL "")
 
-  # The sysroot tree was created via https://github.com/donhinton/misc/blob/master/scripts/export_docker_filesystem.sh
-  set(sysroot "/tmp/docker/ubuntu")
-  set(triple "x86_64-unknown-linux-gnu")
-  set(flags "--sysroot=${sysroot} -target ${triple}")
-  set(link-flags "-fuse-ld=lld")
+  set(CLANG_ENABLE_BOOTSTRAP ON CACHE BOOL "")
+  set(CLANG_BOOTSTRAP_CMAKE_ARGS
+    -DCMAKE_SYSROOT=${SYSROOT}
+	  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_LIST_FILE} CACHE STRING "")
+else()
+  get_filename_component(BASE_PATH "${CMAKE_C_COMPILER}" DIRECTORY CACHE)
+  set(CMAKE_AR "${BASE_PATH}/llvm-ar" CACHE STRING "")
+  set(CMAKE_RANLIB "${BASE_PATH}/llvm-ranlib" CACHE STRING "")
+  set(CLANG_TABLEGEN "${BASE_PATH}/clang-tblgen" CACHE STRING "")
+  set(LLVM_TABLEGEN "${BASE_PATH}/llvm-tblgen" CACHE STRING "")
+  set(_LLVM_CONFIG_EXE "${BASE_PATH}/llvm-config" CACHE STRING "")
+  set(CMAKE_LINKER "${BASE_PATH}/lld" CACHE STRING "")
 
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flags}" CACHE STRING "")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flags}" CACHE STRING "")
+  set(CMAKE_SYSTEM_NAME "Linux" CACHE STRING "" FORCE)
 
-  set(LLVM_DEFAULT_TARGET_TRIPLE "${triple}" CACHE STRING "" FORCE)
+  set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+  set(LLVM_ENABLE_PROJECTS "clang;libcxx;libcxxabi;libunwind;lld" CACHE STRING "")
 
-  set(LIBCXX_TARGET_TRIPLE "${triple}" CACHE STRING "" FORCE)
-  set(LIBCXX_SYSROOT "${sysroot}" CACHE STRING "" FORCE)
-  set(LIBCXX_CFLAGS "${flags}" CACHE STRING "" FORCE)
+  set(LLVM_DEFAULT_TARGET_TRIPLE "x86_64-unknown-linux-gnu" CACHE STRING "" FORCE)
+  set(CMAKE_C_COMPILER_ARG1 "-target ${LLVM_DEFAULT_TARGET_TRIPLE}" CACHE STRING "" FORCE)
+  set(CMAKE_CXX_COMPILER_ARG1 "-target ${LLVM_DEFAULT_TARGET_TRIPLE}" CACHE STRING "" FORCE)
+  set(CMAKE_STATIC_LINKER_FLAGS "-format gnu" CACHE STRING "")
 
-  # These are needed/used for compiler tests, e.g., include files and flags.
-  set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${flags}" CACHE STRING "" FORCE)
-  set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${link-flags}" CACHE STRING "" FORCE)
-
-  set(CMAKE_STATIC_LINKER_FLAGS "-format gnu" CACHE STRING "" FORCE)
-
-  set(LLVM_ENABLE_LLD ON CACHE STRING "" FORCE)
-
-  set(LLVM_ENABLE_ZLIB OFF CACHE BOOL "" FORCE)
+  # need to test for this
+  set(LLVM_ENABLE_ZLIB OFF CACHE BOOL "")
 
   # Force gcc lookup at runtime -- otherwise Darwin will default to 4.2.1.
-  set(GCC_INSTALL_PREFIX "/usr" CACHE STRING "" FORCE)
+  set(GCC_INSTALL_PREFIX "/usr" CACHE STRING "")
 
   # Changing an RPATH from the build tree is not supported with the
   # Ninja generator unless on an ELF-based platform.  This might
   # require changes to llvm cmake files at some point.
   if(CMAKE_HOST_APPLE AND CMAKE_GENERATOR STREQUAL "Ninja")
-    set(CMAKE_BUILD_WITH_INSTALL_RPATH ON CACHE BOOL "" FORCE)
+    set(CMAKE_BUILD_WITH_INSTALL_RPATH ON CACHE BOOL "")
   endif()
 
-
-  set(CMAKE_SYSTEM_PREFIX_PATH "${sysroot}" CACHE STRING "" FORCE)
   # Here is where the target environment located.
-  SET(CMAKE_FIND_ROOT_PATH "${sysroot}" CACHE STRING "" FORCE)
+  SET(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}" CACHE STRING "")
 
-  # adjust the default behaviour of the FIND_XXX() commands:
-  # search headers and libraries in the target environment, search 
-  # programs in the host environment
-  set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER CACHE STRING "" FORCE)
-  set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY CACHE STRING "" FORCE)
-  set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY CACHE STRING "" FORCE)
+  # Adjust the default behaviour of the FIND_XXX() commands:
+  # search headers and libraries in the target environment, search
+  # programs in the host environment.
+  set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER CACHE STRING "")
+  set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY CACHE STRING "")
+  set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY CACHE STRING "")
 endif()
