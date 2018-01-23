573522696e2a914424caa699ca1280464ec0a9a2
Don Hinton <hintonda@gmail.com>
2018-01-23 10:53:45 -0800

    Add polly.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -264,3 +264,53 @@ else
   git config svn-remote.svn.fetch :refs/remotes/origin/master
   git svn rebase -l  # -l avoids fetching ahead of the git mirror.
 fi
+
+# polly
+PROJ=polly
+SUBDIR=${ROOT_DIR}
+#SUBDIR=${ROOT_DIR}/llvm/tools
+PROJ_DIR=${SUBDIR}/${PROJ}
+if [ -d ${PROJ_DIR} ] ; then
+  cd ${PROJ_DIR}
+  MSG=$(git stash)
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  ${ROOT_DIR}/llvm/utils/git-svn/git-svnup
+  if [ "$MSG" != "No local changes to save" ]; then
+    git stash pop
+	fi
+  if [ "$BRANCH" != "master" ]; then
+    git rebase master
+  fi
+else
+  cd ${SUBDIR}
+  git clone https://git.llvm.org/git/${PROJ}.git
+  cd ${PROJ_DIR}
+  git svn init https://llvm.org/svn/llvm-project/${PROJ}/trunk --username=dhinton
+  git config svn-remote.svn.fetch :refs/remotes/origin/master
+  git svn rebase -l  # -l avoids fetching ahead of the git mirror.
+fi
+
+# test-suite
+PROJ=test-suite
+SUBDIR=${ROOT_DIR}
+#SUBDIR=${ROOT_DIR}/llvm/tools
+PROJ_DIR=${SUBDIR}/${PROJ}
+if [ -d ${PROJ_DIR} ] ; then
+  cd ${PROJ_DIR}
+  MSG=$(git stash)
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  ${ROOT_DIR}/llvm/utils/git-svn/git-svnup
+  if [ "$MSG" != "No local changes to save" ]; then
+    git stash pop
+	fi
+  if [ "$BRANCH" != "master" ]; then
+    git rebase master
+  fi
+else
+  cd ${SUBDIR}
+  git clone https://git.llvm.org/git/${PROJ}.git
+  cd ${PROJ_DIR}
+  git svn init https://llvm.org/svn/llvm-project/${PROJ}/trunk --username=dhinton
+  git config svn-remote.svn.fetch :refs/remotes/origin/master
+  git svn rebase -l  # -l avoids fetching ahead of the git mirror.
+fi
